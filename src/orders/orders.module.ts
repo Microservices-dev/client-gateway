85cb356b8d64ed0d62ce0a62be7d11e592548be7
Carlos Ramirez <carlos@extrasistemas.com>
2024-04-10 00:00:18 -0600

    feat: add nats server for connection with ms

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, ORDERS_SERVICE } from 'src/config';
+import { envs, NATS_SERVICE } from 'src/config';
 
 @Module({
   controllers: [OrdersController],
   imports: [
     ClientsModule.register([
       {
-        name: ORDERS_SERVICE,
-        transport: Transport.TCP,
+        name: NATS_SERVICE,
+        transport: Transport.NATS,
         options: {
-          host: envs.ordersMicroserviceHost,
-          port: envs.ordersMicroservicePort,
+          servers: envs.natsServers,
         },
       },
     ]),
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -14,19 +14,17 @@ import {
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common';
-import { PRODUCT_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { NATS_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.productsClient
+    return this.natsClient
       .send({ cmd: 'find_all_products' }, { ...paginationDto })
       .pipe(
         catchError((err) => {
@@ -37,14 +35,14 @@ export class ProductsController {
 
   @Get(':id')
   async findOne(@Param('id') id: number) {
-    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+    return this.natsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
     );
     // try {
     //   const product = await firstValueFrom(
-    //     this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+    //     this.natsClient.send({ cmd: 'find_one_product' }, { id }),
     //   );
     //   return product;
     // } catch (error) {
@@ -55,7 +53,7 @@ export class ProductsController {
   @Post()
   @HttpCode(201)
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient
+    return this.natsClient
       .send({ cmd: 'create_product' }, { ...createProductDto })
       .pipe(
         catchError((err) => {
@@ -66,7 +64,7 @@ export class ProductsController {
 
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsClient
+    return this.natsClient
       .send({ cmd: 'update_product' }, { id, ...updateProductDto })
       .pipe(
         catchError((err) => {
@@ -78,7 +76,7 @@ export class ProductsController {
   @Delete(':id')
   @HttpCode(204)
   delete(@Param('id') id: number): void {
-    this.productsClient.send({ cmd: 'remove_product' }, { id }).pipe(
+    this.natsClient.send({ cmd: 'remove_product' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
